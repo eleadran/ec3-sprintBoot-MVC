71e944ff56f05f9846e6f7d619646392018ec713
adrian infantes <eleazarbarrientos330@gmail.com>
2025-03-15 19:36:59 -0500

    repositorio actualizado

---
+++ src/main/java/pe/edu/idat/ec3_sprintBoot_MVC/Ec3SprintBootMvcApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec3_sprintBoot_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3SprintBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3SprintBootMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/ec3_sprintBoot_MVC/Ec3SprintBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec3_sprintBoot_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3SprintBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
